eadb9de8a33bbf57fa96d942b59fa0acbdd80928
Leslie Baz <lcbaz15@gmail.com>
2018-01-12 17:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,3 +55,27 @@ function getIssues(data) {
 function displayIssue(issue) {
   $('#issues').append(issue.template())
 }
+
+//Fetch and show repo info
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
+}
+
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
+}
+
+function getToken() {
+  return ''
+}
